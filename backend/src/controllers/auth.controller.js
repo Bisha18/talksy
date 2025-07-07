76c675fd0ef8331bc2c9402c0b497412a01eb5a6
bishal <d.bishalpaul@gmail.com>
2025-07-07 19:56:19 +0530

    profilePic feature added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/util.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
@@ -72,3 +73,23 @@ export const logout = (req, res) => {
     return res.status(500).json({message:"Something went wrong"})
   }
 };  
+
+export const updateProfile = async(req,res)=>{
+  try {
+     const {profilePic} = req.body;
+     const userId = req.user._id;
+
+     if(!profilePic){
+      return res.status(400).json({message:"Profile picture is required"})
+     }
+
+     const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+     const updatedUser = await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true})
+
+     return res.status(200).json({message:"Profile picture updated successfully",updatedUser})  
+  } catch (error) {
+     console.log(error);
+     return res.status(500).json({message:"Something went wrong"})
+  }
+}
