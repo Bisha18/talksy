15a3fa988b9c7f37d26a19f9325dd8aeed8a13ed
bishal <d.bishalpaul@gmail.com>
2025-07-07 18:39:36 +0530

    signup added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,19 @@
 import express from 'express'
 import authRoutes from './routes/auth.route.js'
+import dotenv from 'dotenv'
+import connectDB from './lib/db.js'
+import cookieParser from 'cookie-parser'
+dotenv.config()
+
+const port = process.env.PORT || 5001
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
 app.use('/api/auth',authRoutes);
 
-app.listen(5001,()=>{
+app.listen(port,()=>{
   console.log("server is running on port 5001");
+  connectDB();
 })
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = Router();
 
-router.get('/sigin',(req,res)=>{
-  res.send('sigin route');
-})
+router.get('/login',login)
+router.post('/signup',signup)
+router.get('/logout',logout)
 
 export default router;
